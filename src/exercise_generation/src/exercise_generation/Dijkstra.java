64a942d23965bd319a3a3b59deb3177778173e13
Zoltan Sojtory <2442602s@student.gla.ac.uk>
2022-11-08 21:59:08 +0000

    further implementation of dijkstra

--- src/exercise_generation/src/exercise_generation/Dijkstra.java
+++ src/exercise_generation/src/exercise_generation/Dijkstra.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
+
+import exercise_generation.WeightedGraph.Edge;
 
 public class Dijkstra {
 	public int vertices;
@@ -32,7 +35,7 @@ public class Dijkstra {
 	public LinkedHashMap<Integer, Integer> generateDistances(){
 		LinkedHashMap<Integer, Integer> result = new LinkedHashMap<Integer, Integer>();
 		result.put(0, 0);
-		for(int i = 1; i <= this.vertices; i++) {
+		for(int i = 1; i < this.vertices; i++) {
 			while(true) {
 				int randomNum = ThreadLocalRandom.current().nextInt(1, this.maxDistance);
 				if (result.containsValue(randomNum) == false) {
@@ -59,16 +62,50 @@ public class Dijkstra {
 		List<Entry<Integer, Integer>> entriesList = new ArrayList<Entry<Integer, Integer>>(entries);
 		Collections.sort(entriesList, valueComparator);
 		LinkedHashMap<Integer, Integer> result = new LinkedHashMap<Integer, Integer>(entriesList.size());
-		for(Entry<Integer, Integer> entry : entriesList){
-            result.put(entry.getKey(), entry.getValue());
+		for(Entry<Integer, Integer> e : entriesList){
+            result.put(e.getKey(), e.getValue());
         }
 		return result;
 	}
 	
 	public WeightedGraph.Graph generateGraph(){
 		WeightedGraph.Graph result = new WeightedGraph.Graph(this.vertices);
+		LinkedList<Integer> verticesInGraph = new LinkedList<Integer>();
 		//connect vertices
-		
+		boolean first = true;
+		for (Entry<Integer, Integer> entry : this.shortestDistance.entrySet()) {
+			if (entry.getValue() == 0) {
+				continue;
+			}
+			else if (first) {
+				result.addEdge(0, entry.getKey(), entry.getValue());
+				verticesInGraph.add(entry.getKey());
+				first = false;
+			}
+			else if (this.relaxations > 0) {
+				//either add to start or another node based on chance and edge relaxations remaining
+				int chance = ThreadLocalRandom.current().nextInt(1, 7);
+				if (chance == 1) {
+					result.addEdge(0, entry.getKey(), entry.getValue());
+					verticesInGraph.add(entry.getKey());
+				}
+				else {
+					int randomNode = verticesInGraph.get(ThreadLocalRandom.current().nextInt(0, verticesInGraph.size()));
+					result.addEdge(randomNode, entry.getKey(), entry.getValue() - this.shortestDistance.get(randomNode));
+					verticesInGraph.add(entry.getKey());
+					this.relaxations --;
+				}
+			}
+			else {
+				result.addEdge(0, entry.getKey(), entry.getValue());
+				verticesInGraph.add(entry.getKey());
+			}
+			
+			//add more edges to get to no more edge relaxations
+			
+			//optionally add decorator edges
+			
+		}
 		return result;
 	}
 	
@@ -77,6 +114,7 @@ public class Dijkstra {
 			Dijkstra test = new Dijkstra(7, 5);
 			System.out.println(test.maxDistance);
 			System.out.println(test.shortestDistance);
+			test.graph.printGraph();
 		}
 	}
 }
