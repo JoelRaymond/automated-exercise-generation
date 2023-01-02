333cda4e346e2fa917e1db048c97fc6c4e5acfbd
Eszti Lenovo <zsojtory1@gmail.com>
2023-01-02 23:21:11 +0100

    step-by-step dijkstra solution

--- src/exercise_generation/src/exercise_generation/Visualiser.java
+++ src/exercise_generation/src/exercise_generation/Visualiser.java
@@ -4,6 +4,7 @@ import java.awt.Desktop;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -111,6 +112,86 @@ public class Visualiser {
 		}
 	}
 	
+	public static void generateFullGraphSolution(Dijkstra d, String filename, boolean pdf) {
+		try {
+        	FileWriter f = new FileWriter(filename);
+        	f.write("\\documentclass{article}\n");
+        	f.write("\\usepackage{tikz}\n");
+        	f.write("\\usepackage{verbatim}\n");
+        	f.write("\\usetikzlibrary{arrows,shapes}\n");
+        	f.write("\\begin{document}\n");
+        	f.write("\\pgfdeclarelayer{background}\n");
+        	f.write("\\pgfsetlayers{background,main}\n");
+        	f.write("\\tikzstyle{vertex}=[circle,fill=black!25,minimum size=20pt,inner sep=0pt]\n");
+        	f.write("\\tikzstyle{selected vertex} = [vertex, fill=red!24]\n");
+        	f.write("\\tikzstyle{edge} = [draw,thick,-]\n");
+        	f.write("\\tikzstyle{weight} = [font=\\small]\n");
+        	f.write("\\tikzstyle{selected edge} = [draw,line width=5pt,-,red!50]");
+        	f.write("\\begin{flushleft}\n");
+        	f.write("\\textbf{Solution}: These following distances are found, by Dijkstra's algorithm.\n");
+        	f.write("\\end{flushleft}\n");
+        	
+        	ArrayList<Integer> S = new ArrayList<>();
+        	S.add(1);
+        	for (int c=1; c<d.getVertices()+1; c++) {
+	        	f.write("\\begin{center}\n");
+	        	f.write("\\begin{tabular}{| c c c |}\n");
+	        	f.write("\\hline\n");
+	        	f.write("vertex & shortest path & length \\\\\n");
+	        	f.write("\\hline\\hline\n");
+	        	for (int i=2; i<=d.getVertices(); i++) {
+	        		f.write("{$v_{" + Integer.toString(i) + "}$} & $" + 
+	        				d.getShortestPaths().get(i-1) + "$ & " + 
+	        				Integer.toString(d.getShortestDistance().get(i-1)) + "\\\\ \n");
+	        	}
+	        	f.write("\\hline\n");
+	        	f.write("\\end{tabular}\n");
+	        	f.write("\\end{center}\n");
+	        	f.write("\\begin{tikzpicture}[auto]\n");
+	        	f.write("\\def \\radius {7cm}\n");
+	        	int n = d.getGraph().vertices;
+	        	for (int i=1; i<=n; i++) {
+	        		f.write("\\node[vertex] (" + Integer.toString(i) 
+	        		+ ") at ({360/" + Integer.toString(n) + " * (" + Integer.toString(i-1) 
+	        		+ " )}:\\radius) {$v_{" + Integer.toString(i) + "}$};\r\n");
+	        	}
+	        	for (int i=0; i<n; i++) {
+	        		LinkedList<Edge> list = d.getGraph().dirAdjacencylist[i];
+	        		for (int j=0; j<list.size(); j++) {
+	        			f.write("\\path[edge] (" + Integer.toString(i+1) 
+	        			+ ") -- node[weight] {$\\textcolor{red}{" + Integer.toString(list.get(j).weight) 
+	        			+ "}$} (" + Integer.toString(list.get(j).end+1) + ");\r\n");
+	        		}
+	        	}
+	        	
+	        	for (Integer val : S) {
+	        		f.write("\\path node[selected vertex] at (" + Integer.toString(val) + 
+	        				") {$v_{" + Integer.toString(val) + "}$};\n");
+	        	}
+	        	S.add(c+1);
+	        	
+	        	f.write("\\end{tikzpicture}\n");
+	        	f.write("\\pagebreak\n");
+        	}
+        	f.write("\\end{document}\n");
+        	f.close();
+        }
+        catch (IOException e) {
+        	System.out.println("Error");
+        	e.printStackTrace();
+        }
+        
+		if (pdf) {
+	        TeXOptions options = TeXOptions.consoleAppOptions(TeXConfig.objectLaTeX());
+	        options.setInputWorkingDirectory(new InputFileSystemDirectory("solutions/tex"));
+	        options.setOutputWorkingDirectory(new OutputFileSystemDirectory("solutions/pdf"));
+	        options.setTerminalOut(new OutputMemoryTerminal());
+	        options.setSaveOptions(new PdfSaveOptions());
+	        
+	        new TeXJob (filename, new PdfDevice(), options).run();
+		}
+	}
+	
 	public static void generateString(KMP k, String filename, boolean pdf) {
 		try {
         	FileWriter f = new FileWriter(filename);
@@ -142,6 +223,50 @@ public class Visualiser {
 	}
 	
 	public static void generateStringSolution(KMP k, String filename, boolean pdf) {
+		try {
+        	FileWriter f = new FileWriter(filename);
+        	f.write("\\documentclass{article}\n");
+        	f.write("\\begin{document}\n");
+        	f.write("\\textbf{Solution}: The border table is given by:\n");
+        	f.write("\\begin{center}\n");
+        	
+        	String c = "|c";
+        	String j = "j";
+        	String b = "B(j)";
+        	int border[] = k.constructBorderTable();
+        	for(int i=0; i<k.getLength(); i++) {
+        		c += "|c";
+        		j += " & " + Integer.toString(i);
+        		b += " & " + Integer.toString(border[i]);
+        	}
+        	f.write("\\begin{tabular}{" + c + "|}\n");
+        	f.write("\\hline\n");
+        	f.write(j + " \\\\\n");
+        	f.write("\\hline\n");
+        	f.write(b + " \\\\\n");
+        	f.write("\\hline\n");
+        	f.write("\\end{tabular}\n");
+        	f.write("\\end{center}\n");
+        	f.write("\\end{document}\n");
+        	f.close();
+        }
+        catch (IOException e) {
+        	System.out.println("Error");
+        	e.printStackTrace();
+        }
+        
+		if (pdf) {
+	        TeXOptions options = TeXOptions.consoleAppOptions(TeXConfig.objectLaTeX());
+	        options.setInputWorkingDirectory(new InputFileSystemDirectory("solutions/tex"));
+	        options.setOutputWorkingDirectory(new OutputFileSystemDirectory("solutions/pdf"));
+	        options.setTerminalOut(new OutputMemoryTerminal());
+	        options.setSaveOptions(new PdfSaveOptions());
+	        
+	        new TeXJob (filename, new PdfDevice(), options).run();
+		}
+	}
+	
+	public static void generateFullStringSolution(KMP k, String filename, boolean pdf) {
 		try {
         	FileWriter f = new FileWriter(filename);
         	f.write("\\documentclass{article}\n");
@@ -238,11 +363,25 @@ public class Visualiser {
     	}
     	
     	boolean sol = false;
+    	boolean fullSol = false;
     	System.out.println("Would you like to generate solutions? (Y/N)");
     	while (true) {
         	String solString = sc.nextLine();
         	if (solString.equals("Y")) {
         		sol = true;
+        		
+            	System.out.println("Would you like to generate step-by-step solutions? (Y/N)");
+            	while (true) {
+                	String fullSolString = sc.nextLine();
+                	if (fullSolString.equals("Y")) {
+                		fullSol = true;
+                		break;
+                	}
+                	else if (fullSolString.equals("N")) {
+                		fullSol = false;
+                		break;
+                	}
+            	}
         		break;
         	}
         	else if (solString.equals("N")) {
@@ -272,16 +411,22 @@ public class Visualiser {
     	for(int i = 0; i <dijkstraLimit; i++) {
 			Dijkstra test = new Dijkstra(v, e);
 			generateGraph(test, "exercises/tex/dijkstra"+Integer.toString(i+1)+".tex", pdf);
-			if (sol) {
+			if (sol && !fullSol) {
 				generateGraphSolution(test, "solutions/tex/dijkstra_answer"+Integer.toString(i+1)+".tex", pdf);
+			}
+			if (fullSol) {
+				generateFullGraphSolution(test, "solutions/tex/dijkstra_answer"+Integer.toString(i+1)+".tex", pdf);
 			}
 		}
     	
     	for(int i = 0; i <kmpLimit; i++) {
 			KMP test = new KMP(s, lb, o);
 			generateString(test, "exercises/tex/kmp"+Integer.toString(i+1)+".tex", pdf);
-			if (sol) {
+			if (sol && !fullSol) {
 				generateStringSolution(test, "solutions/tex/kmp_answer"+Integer.toString(i+1)+".tex", pdf);
+			}
+			if (fullSol) {
+				generateFullStringSolution(test, "solutions/tex/kmp_answer"+Integer.toString(i+1)+".tex", pdf);
 			}
 		}
     	
