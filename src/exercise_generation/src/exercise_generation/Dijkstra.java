0b34cde645660fd18cd9821805c1fde8a0cde979
Zoltan Sojtory <2442602s@student.gla.ac.uk>
2023-03-05 17:46:50 +0000

    more implementation and formatting

--- src/exercise_generation/src/exercise_generation/Dijkstra.java
+++ src/exercise_generation/src/exercise_generation/Dijkstra.java
@@ -19,7 +19,7 @@ public class Dijkstra {
 	private LinkedHashMap<Integer, Integer> shortestDistance;
 	private HashMap<Integer, String> shortestPaths;
 	private WeightedGraph.Graph graph;
-	
+
 	public int getVertices() {
 		return vertices;
 	}
@@ -51,8 +51,7 @@ public class Dijkstra {
 	public void setShortestDistance(LinkedHashMap<Integer, Integer> shortestDistance) {
 		this.shortestDistance = shortestDistance;
 	}
-	
-	
+
 	public void setShortestPaths(HashMap<Integer, String> shortestPaths) {
 		this.shortestPaths = shortestPaths;
 	}
@@ -72,34 +71,32 @@ public class Dijkstra {
 	public Dijkstra(int vertices, int relaxations) {
 		if (vertices > 0) {
 			this.vertices = vertices;
-		}
-		else {
+		} else {
 			throw new IllegalArgumentException("Illegal vertex input.");
 		}
-		
+
 		int possibleRelaxations = getRelaxations(vertices);
 		if (relaxations <= possibleRelaxations) {
 			this.relaxations = relaxations;
-		}
-		else {
+		} else {
 			throw new IllegalArgumentException("Too many relaxations.");
 		}
-		
+
 		this.maxDistance = calculateMaxDistance();
 		this.shortestDistance = generateDistances();
 		this.shortestPaths = new HashMap<Integer, String>();
 		this.graph = generateGraph();
 	}
-	
-	public int calculateMaxDistance(){
+
+	public int calculateMaxDistance() {
 		return this.vertices * 3;
 	}
-	
-	public LinkedHashMap<Integer, Integer> generateDistances(){
+
+	public LinkedHashMap<Integer, Integer> generateDistances() {
 		LinkedHashMap<Integer, Integer> result = new LinkedHashMap<Integer, Integer>();
 		result.put(0, 0);
-		for(int i = 1; i < this.vertices; i++) {
-			while(true) {
+		for (int i = 1; i < this.vertices; i++) {
+			while (true) {
 				int randomNum = ThreadLocalRandom.current().nextInt(1, this.maxDistance);
 				if (result.containsValue(randomNum) == false) {
 					result.put(i, randomNum);
@@ -110,43 +107,43 @@ public class Dijkstra {
 		result = sortMap(result);
 		return result;
 	}
-	
-	Comparator<Entry<Integer, Integer>> valueComparator = new Comparator<Entry<Integer,Integer>>() {
-		 @Override
-		 public int compare(Entry<Integer, Integer> ent1, Entry<Integer, Integer> ent2) {
-		     Integer val1 = ent1.getValue();
-		     Integer val2 = ent2.getValue();
-		     return val1.compareTo(val2);
-		 }
+
+	Comparator<Entry<Integer, Integer>> valueComparator = new Comparator<Entry<Integer, Integer>>() {
+		@Override
+		public int compare(Entry<Integer, Integer> ent1, Entry<Integer, Integer> ent2) {
+			Integer val1 = ent1.getValue();
+			Integer val2 = ent2.getValue();
+			return val1.compareTo(val2);
+		}
 	};
-	
-	public LinkedHashMap<Integer, Integer> sortMap(Map<Integer, Integer> map){
+
+	public LinkedHashMap<Integer, Integer> sortMap(Map<Integer, Integer> map) {
 		Set<Entry<Integer, Integer>> entries = map.entrySet();
 		List<Entry<Integer, Integer>> entriesList = new ArrayList<Entry<Integer, Integer>>(entries);
 		Collections.sort(entriesList, valueComparator);
 		LinkedHashMap<Integer, Integer> result = new LinkedHashMap<Integer, Integer>(entriesList.size());
-		for(Entry<Integer, Integer> e : entriesList){
-            result.put(e.getKey(), e.getValue());
-        }
+		for (Entry<Integer, Integer> e : entriesList) {
+			result.put(e.getKey(), e.getValue());
+		}
 		return result;
 	}
-	
+
 	public static int getRelaxations(int v) {
-		int n = v-2;
-		return (n * (n + 1))/2;
+		int n = v - 2;
+		return (n * (n + 1)) / 2;
 	}
-	
-	public WeightedGraph.Graph generateGraph(){
+
+	public WeightedGraph.Graph generateGraph() {
 		WeightedGraph.Graph result = new WeightedGraph.Graph(this.vertices);
 		HashMap<Integer, Integer> relaxedTo = new HashMap<Integer, Integer>();
 		HashMap<Integer, Integer> relaxedWeight = new HashMap<Integer, Integer>();
 		HashMap<Integer, List<Integer>> vertexToPath = new HashMap<>();
 		HashMap<Integer, Integer> prevDist = new HashMap<>();
-		
+
 		int possibleRelaxations = getRelaxations(this.vertices);
-		//connect vertices
+		// connect vertices
 		boolean first = true;
-		int maxRelaxation = this.vertices-2;
+		int maxRelaxation = this.vertices - 2;
 		NaryTreeNode root = new NaryTreeNode(0, 0);
 		int maxLevel = 0;
 		for (Entry<Integer, Integer> entry : this.shortestDistance.entrySet()) {
@@ -155,37 +152,34 @@ public class Dijkstra {
 				this.shortestPaths.put(entry.getKey(), "v_{1}");
 				vertexToPath.put(entry.getKey(), Arrays.asList(entry.getKey()));
 				continue;
-			}
-			else if (first) {
+			} else if (first) {
 				result.addEdge(0, entry.getKey(), entry.getValue());
-				this.shortestPaths.put(entry.getKey(), this.shortestPaths.get(0) + 
-						" \\rightarrow v_{" + Integer.toString(entry.getKey()+1) + "}");
+				this.shortestPaths.put(entry.getKey(),
+						this.shortestPaths.get(0) + " \\rightarrow v_{" + Integer.toString(entry.getKey() + 1) + "}");
 				first = false;
 				root.addChild(entry.getKey());
 				maxLevel = 1;
 				List<Integer> path = new ArrayList<>(vertexToPath.get(0));
 				path.add(entry.getKey());
 				vertexToPath.put(entry.getKey(), path);
-			}
-			else {
+			} else {
 				int chosenLevel = 0;
-				//System.out.println("possible");
-				//System.out.println(possibleRelaxations);
-				//System.out.println("needed");
-				//System.out.println(relaxationsNeeded);
-				//System.out.println("maxrelax");
-				//System.out.println(maxRelaxation);
+				// System.out.println("possible");
+				// System.out.println(possibleRelaxations);
+				// System.out.println("needed");
+				// System.out.println(relaxationsNeeded);
+				// System.out.println("maxrelax");
+				// System.out.println(maxRelaxation);
 				int disparity = possibleRelaxations - this.relaxations;
 				if (disparity < maxRelaxation) {
 					chosenLevel = maxLevel;
+				} else {
+					// System.out.println("count");
+					// System.out.println(count);
+					chosenLevel = ThreadLocalRandom.current().nextInt(maxLevel + 1);
 				}
-				else {
-					//System.out.println("count");
-					//System.out.println(count);
-					chosenLevel = ThreadLocalRandom.current().nextInt(maxLevel+1);
-				}
-				//System.out.println("chosen");
-				//System.out.println(chosenLevel);
+				// System.out.println("chosen");
+				// System.out.println(chosenLevel);
 				List<NaryTreeNode> valuesAtLevel = root.getNodesAtLevel(root, chosenLevel);
 				NaryTreeNode startNode = valuesAtLevel.get(ThreadLocalRandom.current().nextInt(valuesAtLevel.size()));
 				int start = startNode.val;
@@ -194,67 +188,63 @@ public class Dijkstra {
 				startNode.addChild(entry.getKey());
 				if (chosenLevel == maxLevel) {
 					maxLevel++;
-				}
-				else {
+				} else {
 					possibleRelaxations -= maxRelaxation;
 					maxRelaxation--;
 				}
-				this.shortestPaths.put(entry.getKey(), this.shortestPaths.get(start) + 
-						" \\rightarrow v_{" + Integer.toString(entry.getKey()+1) + "}");
+				this.shortestPaths.put(entry.getKey(), this.shortestPaths.get(start) + " \\rightarrow v_{"
+						+ Integer.toString(entry.getKey() + 1) + "}");
 				List<Integer> path = new ArrayList<>(vertexToPath.get(start));
 				path.add(entry.getKey());
 				vertexToPath.put(entry.getKey(), path);
-				
+
 			}
 		}
 		for (Integer v : vertexToPath.keySet()) {
 			int list_size = vertexToPath.get(v).size();
 			if (list_size > 2) {
-				relaxedTo.put(v, vertexToPath.get(v).get(list_size-3));
+				relaxedTo.put(v, vertexToPath.get(v).get(list_size - 3));
 			}
 		}
-		//System.out.println(vertexToPath);
-		//add more edges to get to no more edge relaxations
+		// System.out.println(vertexToPath);
+		// add more edges to get to no more edge relaxations
 		while (this.relaxations > 0) {
 
 			Object[] vertices = relaxedTo.keySet().toArray();
 			Object randVert = vertices[ThreadLocalRandom.current().nextInt(vertices.length)];
 			int start = (int) randVert;
 			int end = relaxedTo.get(start);
-				
+
 			int weightIncrease = ThreadLocalRandom.current().nextInt(1, 4);
-			
+
 			int weight;
 			if (relaxedWeight.containsKey(start)) {
-				weight = relaxedWeight.get(start) + (prevDist.get(start) - this.shortestDistance.get(end)) 
+				weight = relaxedWeight.get(start) + (prevDist.get(start) - this.shortestDistance.get(end))
 						+ weightIncrease;
-			}
-			else {
+			} else {
 				weight = (this.shortestDistance.get(start) - this.shortestDistance.get(end)) + weightIncrease;
 			}
 			relaxedWeight.put(start, weight);
-			
+
 			prevDist.put(start, this.shortestDistance.get(end));
-			
+
 			result.addEdge(start, end, weight);
-			
-			
+
 			List<Integer> path = vertexToPath.get(start);
 			int newRelax = 0;
 			try {
-				newRelax = path.get(path.indexOf(end)-1);
+				newRelax = path.get(path.indexOf(end) - 1);
 				relaxedTo.put(start, newRelax);
-			}
-			catch (Exception e) {
+			} catch (Exception e) {
 				relaxedTo.remove(start);
 			}
 			this.relaxations--;
 		}
 		return result;
 	}
-	
+
 	public static void main(String[] args) {
-		for(int i = 0; i <1; i++) {
+		for (int i = 0; i < 1; i++) {
 			Dijkstra test = new Dijkstra(10, 10);
 			System.out.println(test.maxDistance);
 			System.out.println(test.shortestDistance);
--- src/exercise_generation/src/exercise_generation/WeightedGraph.java
+++ src/exercise_generation/src/exercise_generation/WeightedGraph.java
@@ -3,66 +3,68 @@ package exercise_generation;
 import java.util.LinkedList;
 
 public class WeightedGraph {
-	 static class Edge {
-		 int start;
-		 int end;
-		 int weight;
-	
-		 Edge(int start, int end, int weight) {
-			 this.start = start;
-			 this.end = end;
-			 this.weight = weight;
-		 }
-	 }
+	static class Edge {
+		int start;
+		int end;
+		int weight;
 
-	 static class Graph {
-		 int vertices;
-		 LinkedList<Edge> [] adjacencylist;
-		 LinkedList<Edge> [] dirAdjacencylist;
-	
-		 Graph(int vertices) {
-			 this.vertices = vertices;
-			 adjacencylist = new LinkedList[vertices];
-			 dirAdjacencylist = new LinkedList[vertices];
+		Edge(int start, int end, int weight) {
+			this.start = start;
+			this.end = end;
+			this.weight = weight;
+		}
+	}
 
-			 for (int i = 0; i <vertices ; i++) {
-				 adjacencylist[i] = new LinkedList<>();
-				 dirAdjacencylist[i] = new LinkedList<>();
-			 }
-		 }
+	static class Graph {
+		int vertices;
+		LinkedList<Edge>[] adjacencylist;
+		LinkedList<Edge>[] dirAdjacencylist;
 
-		 public void addEdge(int start, int end, int weight) {
-			 Edge edge = new Edge(start, end, weight);
-			 adjacencylist[start].addFirst(edge); 
-			 
-			 dirAdjacencylist[start].addFirst(edge); 
-			 
-			 Edge edge2 = new Edge(end, start, weight);
-			 adjacencylist[end].addFirst(edge2);
-		 }
-	
-		 public void printGraph(){
-			 for (int i = 0; i <vertices ; i++) {
-				 LinkedList<Edge> list = dirAdjacencylist[i];
-				 for (int j = 0; j <list.size() ; j++) {
-					 System.out.println("vertex-" + (i+1) + " is connected to " + (list.get(j).end+1) + " with weight " + list.get(j).weight);
-				 }
-			 }
-		 }
-	 }
-	 public static void main(String[] args) {
-		 int vertices = 6;
-		 Graph graph = new Graph(vertices);
-		 graph.addEdge(0, 1, 4);
-		 graph.addEdge(0, 2, 3);
-		 graph.addEdge(1, 3, 2);
-		 graph.addEdge(1, 2, 5);
-		 graph.addEdge(2, 3, 7);
-		 graph.addEdge(3, 4, 2);
-		 graph.addEdge(4, 0, 4);
-		 graph.addEdge(4, 1, 4);
-		 graph.addEdge(4, 5, 6);
-		 graph.printGraph();
-		 System.out.println(graph.adjacencylist[0].get(0).end);
-		 }
+		Graph(int vertices) {
+			this.vertices = vertices;
+			adjacencylist = new LinkedList[vertices];
+			dirAdjacencylist = new LinkedList[vertices];
+
+			for (int i = 0; i < vertices; i++) {
+				adjacencylist[i] = new LinkedList<>();
+				dirAdjacencylist[i] = new LinkedList<>();
+			}
+		}
+
+		public void addEdge(int start, int end, int weight) {
+			Edge edge = new Edge(start, end, weight);
+			adjacencylist[start].addFirst(edge);
+
+			dirAdjacencylist[start].addFirst(edge);
+
+			Edge edge2 = new Edge(end, start, weight);
+			adjacencylist[end].addFirst(edge2);
+		}
+
+		public void printGraph() {
+			for (int i = 0; i < vertices; i++) {
+				LinkedList<Edge> list = dirAdjacencylist[i];
+				for (int j = 0; j < list.size(); j++) {
+					System.out.println("vertex-" + (i + 1) + " is connected to " + (list.get(j).end + 1)
+							+ " with weight " + list.get(j).weight);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int vertices = 6;
+		Graph graph = new Graph(vertices);
+		graph.addEdge(0, 1, 4);
+		graph.addEdge(0, 2, 3);
+		graph.addEdge(1, 3, 2);
+		graph.addEdge(1, 2, 5);
+		graph.addEdge(2, 3, 7);
+		graph.addEdge(3, 4, 2);
+		graph.addEdge(4, 0, 4);
+		graph.addEdge(4, 1, 4);
+		graph.addEdge(4, 5, 6);
+		graph.printGraph();
+		System.out.println(graph.adjacencylist[0].get(0).end);
+	}
 }
